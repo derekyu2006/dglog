912db47a1e9171cd156d5458651d571b6e74ff0b
derekyu2006 <derekyu2006@126.com>
2022-06-17 13:22:22 +0800

    Create export.go

---
+++ export.go
@@ -0,0 +1,35 @@
+package dglog
+
+import (
+  "github.com/sirupsen/logrus"
+)
+
+// Tracef logs a message at level Trace on the standard logger.
+func Tracef(format string, args ...interface{}) {
+	logrus.Tracef(format, args...)
+}
+
+// Debugf logs a message at level Debug on the standard logger.
+func Debugf(format string, args ...interface{}) {
+	logrus.Debugf(format, args...)
+}
+
+// Infof logs a message at level Info on the standard logger.
+func Infof(format string, args ...interface{}) {
+	logrus.Infof(format, args...)
+}
+
+// Warnf logs a message at level Warn on the standard logger.
+func Warnf(format string, args ...interface{}) {
+	logrus.Warnf(format, args...)
+}
+
+// Warningf logs a message at level Warn on the standard logger.
+func Warningf(format string, args ...interface{}) {
+	logrus.Warningf(format, args...)
+}
+
+// Errorf logs a message at level Error on the standard logger.
+func Errorf(format string, args ...interface{}) {
+	logrus.Errorf(format, args...)
+}
